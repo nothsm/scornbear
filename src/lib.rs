c74549335b49e0c41b8ad5f32b5e940ff11491f7
Hilal Mufti <hmufti@cs.washington.edu>
2025-08-11 00:35:11 -0700

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -10,36 +10,28 @@ use std::error::{Error};
 use std::io::{self, Read};
 
 /*
- * === scornbear Design ===
- * The scornbear system is designed first and foremost around *simplicity*.
+ * === scornbear design ===
+ * the scornbear program is designed first and foremost around *simplicity*.
  *
- * We use the following criteria guide our design:
- * 1) ** Simplicity **
- * 2) Correctness
- * 3) Hackability
- * 4) Performance.
+ * we use the following criteria guide our design:
+ * 1) ** simplicity **
+ * 2) correctness
+ * 3) hackability
+ * 4) performance.
  *
- * TODO: Improve the writing in this message.
+ * TODO: improve the writing in this message.
  */
 
 /*
- * === scornbear Style ===
- * - "scornbear" should always be written in all lowercase. Never refer to
- *    scornbear as a "system". Always a "program".
- * - Floats with only trailing 0's should be written as "x.". For example, "1.", "3.", "4.".
- * - Convert to String using .to_string() rather than String::from.
- *     Justification: Pipelining is easier with .to_string().
- * - No nested conditionals or nested pattern matching.
- * - We use the OpenBSD commenting style. That is, we use the /* */ delimiters
- *   for comments. All comments should be written in complete sentences with
- *   punctuation.
- *
- *   Here's an example of a comment,
- *   /*
- *    * Your comment goes here.
- *    */.
- *
- *   Small inline comments can be written inline with /*  */.
+ * === scornbear style ===
+ * - "scornbear" should always be written in all lowercase. never refer to
+ *    scornbear as a "system". always a "program".
+ * - floats with only trailing 0's should be written as "x.". for example, "1.", "3.", "4.".
+ * - convert to String using .to_string() rather than String::from.
+ *     why: pipelining is easier with .to_string().
+ * - no nested conditionals or nested pattern matching.
+ * - use the openbsd commenting style. read the code for examples.
+ * - all comments should be written in complete sentences with punctuation.
  */
 
 const LOGO: &str = r"                          _
@@ -189,7 +181,7 @@ fn atom_read(s: &str) -> Result<Expr, &'static str> {
 
     use Expr::{FpLit, IntLit, Symbol};
 
-    /* TODO: This is really ugly. */
+    /* TODO: this is really ugly. */
     match s.parse::<i32>() {
         Ok(n) => Ok(IntLit(n)),
         Err(_) => match s.parse::<f32>() {
@@ -203,9 +195,8 @@ fn atom_read(s: &str) -> Result<Expr, &'static str> {
  * parse the given list of tokens.
  * pre: TODO
  * post: TODO
- * TODO: How the hell was this algorithm derived?
- * TODO: I need to transform this into an iterative procedure using a stack. I
- *       don't want mutable references.
+ * TODO: how the hell was this algorithm derived?
+ * TODO: make this function iterative, using a stack.
  */
 fn tokens_read(ts: &mut VecDeque<String>) -> Result<Expr, &'static str> {
     /*
@@ -261,7 +252,7 @@ fn add_zero_lint(x: Expr) -> Option<String> {
 }
 
 /*
- * TODO
+ * lints TODO
  * pre: TODO
  * post: TODO
  */
@@ -281,7 +272,7 @@ fn mul_one_lint(x: Expr) -> Option<String> {
 }
 
 /*
- * TODO
+ * lints TODO
  * pre: TODO
  * post: TODO
  */
@@ -807,21 +798,18 @@ mod test {
 
     /*
      * TODO:
-     * - [ ] Add rust or emacs macro for faster testing.
-     * - [ ] Faster Emacs expect test updates.
-     * - [ ] Add more formal proofs of correctness.
-     * - [ ] Improve performance via caching/DoD.
-     * - [ ] Write my own trim() implementation?
-     * - [ ] Add better error datatype for Result<...> structs.
-     * - [ ] Simplify Expr datatype.
-     * - [ ] Less verbose pretty printer for Expr.
-     * - [ ] Should the atomic expression property be embedded in the type system?
-     * - [ ] Rename `scornbear` to `libsb`.
-     * - [ ] Add float arms for zero_lint.
-     * - [ ] Reify Mul, Add to Expr (then simplify corresponds rules).
-     * - [ ] Convert String::from to .to_string()
-     * - [ ] Add Rule enum and convert corresponding lints.
-     * - [ ] Add property-based tests.
-     * - [ ] Move test expressions into mod test.
+     * - [ ] add rust or emacs macro for faster testing.
+     * - [ ] faster Emacs expect test updates.
+     * - [ ] add more formal proofs of correctness.
+     * - [ ] improve performance via caching/DoD.
+     * - [ ] write my own trim() implementation?
+     * - [ ] add better error datatype for Result<...> structs.
+     * - [ ] simplify Expr datatype.
+     * - [ ] less verbose pretty printer for Expr.
+     * - [ ] should the atomic expression property be embedded in the type system?
+     * - [ ] rename `scornbear` to `libsb`.
+     * - [ ] add Mul, Add to Expr (then simplify corresponds rules).
+     * - [ ] add Rule enum and convert corresponding lints.
+     * - [ ] add property-based tests.
      */
 }
